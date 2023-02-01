915c0765a077a0eb45893b1123161ef3d2ce4b80
AbdulinLaura <79696059+AbdulinLaura@users.noreply.github.com>
2023-02-01 14:34:18 +0200

    Формирование фреймворка

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,27 +1,29 @@
-import requests
 import pytest
+from lib.my_requests import MyRequests
+import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
 
+@allure.epic("Authorization cases")
 class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"), ("no token")
     ]
+
     def setup_method(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
         }
-
-        self.response1 = requests.post('https://playground.learnqa.ru/api/user/login', data=data)
-
-        self.auth_sid = self.get_cookie(self.response1, "auth_sid")
-        self.token = self.get_headers(self.response1, "x-csrf-token")
-        self.user_from_auth = self.get_json_value(self.response1, "user_id")
-
+        response1 = MyRequests.post("/user/login", data=data)
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_headers(response1, "x-csrf-token")
+        self.user_from_auth = self.get_json_value(response1, "user_id")
+    @allure.description("This test saccessfully authorize user by email and password")
     def test_user_auth(self):
-        response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth",
+        response2 = MyRequests.get(
+            "/user/auth",
             headers={"x-csrf-token": self.token},
             cookies={"auth_sid": self.auth_sid}
         )
@@ -32,18 +34,17 @@ class TestUserAuth(BaseCase):
             "User id from auth method is not equal to user id from check method"
         )
 
-
+    @allure.description("This test check authorization testes status w/o sending auth cookie or token")
     @pytest.mark.parametrize('condition', exclude_params)
-    def test_negativ_auth_check(self, condition):
-        auth_sid = self.response1.cookies.get("auth_sid")
-        token = self.response1.headers.get("x-csrf-token")
+    def test_negative_auth_check(self, condition):
+
         if condition == "no_cookie":
-            response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth", headers={"x-csrf-token": token}
+            response2 = MyRequests.get(
+                "/user/auth", headers={"x-csrf-token": self.token}
             )
         else:
-            response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth", cookies= {"auth_sid":auth_sid}
+            response2 = MyRequests.get(
+                "/user/auth", cookies={"auth_sid": self.auth_sid}
             )
         Assertions.assert_json_value_by_name(
             response2,
--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -2,8 +2,10 @@ import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 import pytest
+import allure
 
 
+@allure.epic("Case of deletion")
 class TestUserDelete(BaseCase):
     user1_email = ""
     user1_password = ""
@@ -22,64 +24,76 @@ class TestUserDelete(BaseCase):
         self.user2_email, self.user2_password, self.user2_id = self.create_new_user(self)
         self.user3_email, self.user3_password, self.user3_id = self.create_new_user(self)
 
+    @allure.story("Negative")
+    @allure.title("Deleting forbidden user")
+    @allure.description("This test shows that users with id 1,2,3,4,5,don't delete")
     def test_remove_user1(self):
-        data = {
-            'email': 'vinkotov@example.com',
-            'password': '1234'
-        }
+        with allure.step("User authorization"):
+            data = {
+                'email': 'vinkotov@example.com',
+                'password': '1234'
+            }
         response = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
         auth_sid = self.get_cookie(response, "auth_sid")
         token = self.get_headers(response, "x-csrf-token")
         user_id = self.get_json_value(response, "user_id")
-
-        response2 = requests.delete(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
-        )
+        with allure.step("Deleted forbidden user"):
+            response2 = requests.delete(
+                f"https://playground.learnqa.ru/api/user/{user_id}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid}
+            )
         assert response2.text == "Please, do not delete test users with ID 1, 2, 3, 4 or 5.", f"User with id {user_id} deleted"
         Assertions.assert_cod_status(response2, 400)
-
-        response3 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid}
-                                 )
+        with allure.step("Check information about user"):
+            response3 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                     headers={"x-csrf-token": token},
+                                     cookies={"auth_sid": auth_sid}
+                                     )
 
         expected_fields = ["id", "username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response3, expected_fields)
 
+    @allure.story("Positive")
+    @allure.title("Deleting a user")
+    @allure.description("This test successfully deleted authorized user")
     def test_remove_user2(self):
-        login_data = {
-            'email': self.user1_email,
-            'password': self.user1_password
-        }
+        with allure.step("User authorization"):
+            login_data = {
+                'email': self.user1_email,
+                'password': self.user1_password
+            }
 
         response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_headers(response2, "x-csrf-token")
-
-        response3 = requests.delete(
-            f"https://playground.learnqa.ru/api/user/{self.user1_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
-        )
+        with allure.step("User deleted"):
+            response3 = requests.delete(
+                f"https://playground.learnqa.ru/api/user/{self.user1_id}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid}
+            )
         Assertions.assert_cod_status(response3, 200)
-
-        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{self.user1_id}",
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid}
-                                 )
+        with allure.step("View user information"):
+            response4 = requests.get(f"https://playground.learnqa.ru/api/user/{self.user1_id}",
+                                     headers={"x-csrf-token": token},
+                                     cookies={"auth_sid": auth_sid}
+                                     )
 
         assert response4.text == "User not found", f"User {self.user1_id} not deleted"
         Assertions.assert_cod_status(response4, 404)
 
+    @allure.story("Negative")
+    @allure.title("Deleting other user")
+    @allure.description("This test successfully deleted not authorized user")
     def test_remove_other_user(self):
-        data_user2 = {
-            'email': self.user2_email,
-            'password': self.user2_password
-        }
+        with allure.step("User authorization"):
+            data_user2 = {
+                'email': self.user2_email,
+                'password': self.user2_password
+            }
         response = requests.post("https://playground.learnqa.ru/api/user/login", data=data_user2)
-
-        response2 = requests.delete(f"https://playground.learnqa.ru/api/user/{self.user3_id}")
+        with allure.step("Delete other user"):
+            response2 = requests.delete(f"https://playground.learnqa.ru/api/user/{self.user3_id}")
         Assertions.assert_cod_status(response2, 400)
